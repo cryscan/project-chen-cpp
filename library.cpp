228ae9563e9dbdecf86a685f356db646e65aa799
cryscan <cryscan@umich.edu>
2021-02-28 23:06:41 -0500

    Get solutions.

--- library.cpp
+++ library.cpp
@@ -9,8 +9,11 @@
 #include <mutex>
 #include <exception>
 
+#include <Eigen/Core>
+
 #include <towr/nlp_formulation.h>
 #include <towr/terrain/examples/height_map_examples.h>
+#include <towr/initialization/gait_generator.h>
 #include <ifopt/ipopt_solver.h>
 
 #include "library.h"
@@ -18,19 +21,16 @@
 
 using Lock = std::unique_lock<std::mutex>;
 
-struct Formulation {
-    towr::NlpFormulation formulation;
-    std::mutex mutex;
-};
-
-struct Solution {
-    std::future<towr::SplineHolder> solution;
-    std::mutex mutex;
-};
-
 struct Session {
-    Formulation formulation;
-    Solution solution;
+    towr::NlpFormulation formulation;
+    std::mutex formulation_mutex;
+
+    struct Solution {
+        std::future<towr::SplineHolder> future;
+        towr::SplineHolder current;
+        bool ready = false;
+    } solution;
+    std::mutex solution_mutex;
 };
 
 struct InvalidSessionError : std::exception {
@@ -56,28 +56,43 @@ void check_session(int session, Lock&) {
         throw InvalidSessionError(session);
 }
 
-std::tuple<Formulation*, Lock> get_session_formulation(int session) {
+std::tuple<towr::NlpFormulation*, Lock> get_formulation(int session) {
     auto lock = std::unique_lock(sessions.mutex);
     try { check_session(session, lock); }
     catch (InvalidSessionError& error) { std::cerr << error.what() << std::endl; }
 
     auto lock_ = std::move(lock);
     auto formulation = &sessions.data[session]->formulation;
-    lock = std::unique_lock(formulation->mutex);
+    lock = std::unique_lock(sessions.data[session]->formulation_mutex);
 
     return std::make_tuple(formulation, std::move(lock));
 }
 
-std::tuple<Solution*, Lock> get_session_solution(int session) {
+std::tuple<Session::Solution*, Lock> get_solution(int session) {
     auto lock = std::unique_lock(sessions.mutex);
     try { check_session(session, lock); }
     catch (InvalidSessionError& error) { std::cerr << error.what() << std::endl; }
 
     auto lock_ = std::move(lock);
     auto solution = &sessions.data[session]->solution;
-    lock = std::unique_lock(solution->mutex);
+    lock = std::unique_lock(sessions.data[session]->solution_mutex);
 
     return std::make_tuple(solution, std::move(lock));
+}
+
+void init_gait_combos(towr::NlpFormulation& formulation,
+                      towr::GaitGenerator::Combos combos,
+                      double duration,
+                      Lock&) {
+    auto ee_count = formulation.model_.dynamic_model_->GetEECount();
+
+    auto gait = towr::GaitGenerator::MakeGaitGenerator(ee_count);
+    gait->SetCombo(static_cast<towr::GaitGenerator::Combos>(combos));
+
+    for (int i = 0; i < ee_count; ++i) {
+        formulation.params_.ee_phase_durations_.push_back(gait->GetPhaseDurations(duration, i));
+        formulation.params_.ee_in_contact_at_start_.push_back(gait->IsInContactAtStart(i));
+    }
 }
 
 int create_session() {
@@ -86,9 +101,11 @@ int create_session() {
     auto session = std::make_unique<Session>();
 
     // TODO: make this more flexible.
-    auto& formulation = session->formulation.formulation;
+    auto& formulation = session->formulation;
     formulation.model_ = towr::RobotModel::Monoped;
     formulation.terrain_ = towr::HeightMap::MakeTerrain(towr::HeightMap::FlatID);
+
+    init_gait_combos(formulation, towr::GaitGenerator::C0, 2.0, lock);
 
     if (iter != sessions.data.end()) {
         *iter = std::move(session);
@@ -111,62 +128,62 @@ void end_session(int session) {
 }
 
 void set_initial_base_linear_position(int session, double x, double y, double z) {
-    auto[formulation, lock] = get_session_formulation(session);
-    formulation->formulation.initial_base_.lin.at(towr::kPos) << x, y, z;
+    auto[formulation, lock] = get_formulation(session);
+    formulation->initial_base_.lin.at(towr::kPos) << x, y, z;
 }
 
 void set_initial_base_linear_velocity(int session, double x, double y, double z) {
-    auto[formulation, lock] = get_session_formulation(session);
-    formulation->formulation.initial_base_.lin.at(towr::kVel) << x, y, z;
+    auto[formulation, lock] = get_formulation(session);
+    formulation->initial_base_.lin.at(towr::kVel) << x, y, z;
 }
 
 void set_initial_base_angular_position(int session, double x, double y, double z) {
-    auto[formulation, lock] = get_session_formulation(session);
-    formulation->formulation.initial_base_.ang.at(towr::kPos) << x, y, z;
+    auto[formulation, lock] = get_formulation(session);
+    formulation->initial_base_.ang.at(towr::kPos) << x, y, z;
 }
 
 void set_initial_base_angular_velocity(int session, double x, double y, double z) {
-    auto[formulation, lock] = get_session_formulation(session);
-    formulation->formulation.initial_base_.ang.at(towr::kVel) << x, y, z;
+    auto[formulation, lock] = get_formulation(session);
+    formulation->initial_base_.ang.at(towr::kVel) << x, y, z;
 }
 
 void set_final_base_linear_position(int session, double x, double y, double z) {
-    auto[formulation, lock] = get_session_formulation(session);
-    formulation->formulation.final_base_.lin.at(towr::kPos) << x, y, z;
+    auto[formulation, lock] = get_formulation(session);
+    formulation->final_base_.lin.at(towr::kPos) << x, y, z;
 }
 
 void set_final_base_linear_velocity(int session, double x, double y, double z) {
-    auto[formulation, lock] = get_session_formulation(session);
-    formulation->formulation.final_base_.lin.at(towr::kVel) << x, y, z;
+    auto[formulation, lock] = get_formulation(session);
+    formulation->final_base_.lin.at(towr::kVel) << x, y, z;
 }
 
 void set_final_base_angular_position(int session, double x, double y, double z) {
-    auto[formulation, lock] = get_session_formulation(session);
-    formulation->formulation.final_base_.ang.at(towr::kPos) << x, y, z;
+    auto[formulation, lock] = get_formulation(session);
+    formulation->final_base_.ang.at(towr::kPos) << x, y, z;
 }
 
 void set_final_base_angular_velocity(int session, double x, double y, double z) {
-    auto[formulation, lock] = get_session_formulation(session);
-    formulation->formulation.final_base_.ang.at(towr::kVel) << x, y, z;
+    auto[formulation, lock] = get_formulation(session);
+    formulation->final_base_.ang.at(towr::kVel) << x, y, z;
 }
 
 void set_initial_end_effector_position(int session, int id, double x, double y) {
-    auto[formulation, lock] = get_session_formulation(session);
-    auto z = formulation->formulation.terrain_->GetHeight(x, y);
-    formulation->formulation.initial_ee_W_[id] << x, y, z;
+    auto[formulation, lock] = get_formulation(session);
+    auto z = formulation->terrain_->GetHeight(x, y);
+    formulation->initial_ee_W_[id] << x, y, z;
 }
 
 towr::SplineHolder async_optimize(int session) {
-    auto[formulation, lock] = get_session_formulation(session);
+    auto[formulation, lock] = get_formulation(session);
 
     ifopt::Problem problem;
     towr::SplineHolder solution;
 
-    for (auto& vars: formulation->formulation.GetVariableSets(solution))
+    for (auto& vars: formulation->GetVariableSets(solution))
         problem.AddVariableSet(vars);
-    for (auto& constrains: formulation->formulation.GetConstraints(solution))
+    for (auto& constrains: formulation->GetConstraints(solution))
         problem.AddConstraintSet(constrains);
-    for (auto& costs: formulation->formulation.GetCosts())
+    for (auto& costs: formulation->GetCosts())
         problem.AddCostSet(costs);
 
     ifopt::IpoptSolver solver;
@@ -177,6 +194,61 @@ towr::SplineHolder async_optimize(int session) {
 }
 
 void start_optimization(int session) {
-    auto[solution, lock] = get_session_solution(session);
-    solution->solution = std::async(async_optimize, session);
+    auto[solution, lock] = get_solution(session);
+    solution->future = std::async(async_optimize, session);
+    solution->ready = false;
+}
+
+bool update_solution(Session::Solution& solution, Lock&) {
+    if (!solution.ready) {
+        if (solution.future.valid()) {
+            solution.current = solution.future.get();
+            solution.ready = true;
+        } else return false;
+    }
+    return true;
+}
+
+bool get_base_linear_position(int session, double time, double* output) {
+    auto[solution, lock] = get_solution(session);
+    if (!update_solution(*solution, lock)) return false;
+
+    auto point = solution->current.base_linear_->GetPoint(time).p();
+    Eigen::Map<Eigen::VectorXd>(output, point.rows(), 1) = point;
+    return true;
+}
+
+bool get_base_angular_position(int session, double time, double* output) {
+    auto[solution, lock] = get_solution(session);
+    if (!update_solution(*solution, lock)) return false;
+
+    auto point = solution->current.base_angular_->GetPoint(time).p();
+    Eigen::Map<Eigen::VectorXd>(output, point.rows(), 1) = point;
+    return true;
+}
+
+bool get_end_effector_position(int session, int id, double time, double* output) {
+    auto[solution, lock] = get_solution(session);
+    if (!update_solution(*solution, lock)) return false;
+
+    auto point = solution->current.ee_motion_.at(id)->GetPoint(time).p();
+    Eigen::Map<Eigen::VectorXd>(output, point.rows(), 1) = point;
+    return true;
+}
+
+bool get_end_effector_force(int session, int id, double time, double* output) {
+    auto[solution, lock] = get_solution(session);
+    if (!update_solution(*solution, lock)) return false;
+
+    auto point = solution->current.ee_force_.at(id)->GetPoint(time).p();
+    Eigen::Map<Eigen::VectorXd>(output, point.rows(), 1) = point;
+    return true;
+}
+
+bool get_end_effector_contact(int session, int id, double time, bool* output) {
+    auto[solution, lock] = get_solution(session);
+    if (!update_solution(*solution, lock)) return false;
+
+    *output = solution->current.phase_durations_.at(id)->IsContactPhase(time);
+    return true;
 }
--- library.h
+++ library.h
@@ -22,6 +22,12 @@ void set_final_base_angular_velocity(int session, double x, double y, double z);
 void set_initial_end_effector_position(int session, int id, double x, double y);
 
 void start_optimization(int session);
+
+bool get_base_linear_position(int session, double time, double* output);
+bool get_base_angular_position(int session, double time, double* output);
+bool get_end_effector_position(int session, int id, double time, double* output);
+bool get_end_effector_force(int session, int id, double time, double* output);
+bool get_end_effector_contact(int session, int id, double time, bool* output);
 }
 
 #endif //PLAYGROUND_LIBRARY_H
